d73b2b85f581c2139902b3fe6b61b51dba268d6e
Lucas Guilherme <lucassguilherme159@gmail.com>
2024-05-30 13:57:30 -0400

    Terminado o Cap7

--- Cap7/MÉTODO DE EXTENSÃO, DICIONÁRIOS, PARÂMETROS OPCIONAIS E DELEGATES/DelegatesFunc.cs
+++ Cap7/MÉTODO DE EXTENSÃO, DICIONÁRIOS, PARÂMETROS OPCIONAIS E DELEGATES/DelegatesFunc.cs
@@ -20,7 +20,16 @@ public static class DelegateFuncTest{
         Console.WriteLine($"A quantidade de palavras da expressão \"Eu escrevo qualquer expressão\" é: ");
         Console.WriteLine(MetodoAnonContaPalavras("Eu escrevo qualquer expressão"));
 
-        
+        System.Console.WriteLine("Delegate Func com 3 entradas e uma saída");
+
+        Func<decimal, decimal, decimal, decimal> Imposto = (salario, perc, aliquota) => {
+            return salario <= 1000 ? 0 : salario * (perc/100) - aliquota;
+        };
+
+        Console.WriteLine ("---- calculo do Imposto ----");
+        System.Console.WriteLine(Imposto(1000, 10, 10));
+        System.Console.WriteLine(Imposto(5000, 27.5M, 80)); //o uso do sufixo M aumenta a precisão dos arredondamentos. armazena até 28 digitos, possue melhor arredondamento do que double. Alta precisão.
+        System.Console.WriteLine(Imposto(23500, 32.5M, 180));
 
     }
 }
